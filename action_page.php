0381e42ed7182ce6b05385557c4e0688c119db28
Vinayak54 <revdekarvinayak54@gmail.com>
2023-11-06 00:05:52 +0530

    checkout page

--- action_page.php
+++ action_page.php
@@ -15,7 +15,7 @@
             margin: 15px;
             text-align: center;
             font-size: 32px;
-            margin-top: 100px;
+            margin-top: 50px;
         }
         .success2{
             padding: 33px;
@@ -23,14 +23,20 @@
             margin: 40px;
             text-align: center;
             font-size: 32px;
-            margin-bottom:100px;
+            margin-bottom:50px;
+        }
+        .center {
+            display: block;
+            margin-left: 550px;
+            margin-right: auto;
+            width: 50%;
         }
     </style>
 </head>
 
 <body>
     <p class=success1> You Order has been placed sucessfully!!!</p>
-     <iframe width="560" height="315" src="https://media.istockphoto.com/id/1396245389/vector/order-confirmed-concept.jpg?s=612x612&w=0&k=20&c=k_BiEGt_01Srsy5z-88OArwoDqPXPYJE7v2XmfnjqeI=" frameborder="0" allowfullscreen></iframe>
+     <iframe class="center" width="560" height="450" src="https://media.istockphoto.com/id/1396245389/vector/order-confirmed-concept.jpg?s=612x612&w=0&k=20&c=k_BiEGt_01Srsy5z-88OArwoDqPXPYJE7v2XmfnjqeI=" frameborder="0" allowfullscreen></iframe>
       </iframe>
     <div>
         <p class=success2>click Here to go to the <a href="index.php">Home Page</a></p>
