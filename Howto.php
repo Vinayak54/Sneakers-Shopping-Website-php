9e8492430b1e2589781e5f2101778fd41e2f345a
Chaitanya Revandkar <110566385+chaiiitanya@users.noreply.github.com>
2023-11-05 22:45:06 +0530

    HowTo.php updated by Chaitanya
    Added CSS Cards and More Articles related to sneaker shopping  in HowTo.php

--- Howto.php
+++ Howto.php
@@ -6,50 +6,169 @@ include ('header.php');
 
 <html lang="en">
 <head>
-<style>
-    #howto{
-    font-family:'Urbanist', sans-serif;;
-    text-align: center;
-    padding-top: 50px;
-  }
+    <style>
+        #howto {
+            font-family: 'Urbanist', sans-serif;
+            text-align: center;
+            padding-top: 50px;
+        }
 
-</style>
+        .video-card {
+            display: inline-block;
+            width: 300px;
+            margin: 10px;
+            text-align: left;
+            border: 1px solid #ccc;
+            padding: 10px;
+        }
+
+        .video-title {
+            font-weight: bold;
+        }
+
+        .video-description {
+            font-size: 14px;
+            color: #888;
+        }
+
+        .video-embed {
+            width: 100%;
+            height: 200px; 
+        }
+
+        .info{
+          text-align: left;
+          margin-left: 110px;
+          padding: 10px;
+        }
+    </style>
 </head>
 
-    <section id="howto" class="section-h1">
+<section id="howto" class="section-h1">
     <h1 style="font-family: 'Urbanist', sans-serif;">HowTo</h1>
     <p>HowTo section provides services to the new customers who are new to the sneaker culture and get help through links and articles ready on our website</p>
-    <br>
-    <p>-----------------------------------------------------------</p>
-    <br>
-    <h2 style="font-family: 'Urbanist', sans-serif;">1. How to choose a perfect size</h2><br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/hJXD46OpiA0" frameborder="0" allowfullscreen></iframe>
-      </iframe>
-      <br>
-      <p>-----------------------------------------------------------</p>
-      <br>
-      <h2 style="font-family: 'Urbanist', sans-serif;">2. How to style Nike Air Force 1</h2><br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/Lp-4TmjsRro" frameborder="0" allowfullscreen></iframe>
-      </iframe>
-      <br>
-      <p>-----------------------------------------------------------</p>
-      <br>
-      <h2 style="font-family: 'Urbanist', sans-serif;">3. How to take care of your Sneakers</h2><br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/xNQRk76JJLI" frameborder="0" allowfullscreen></iframe>
-      </iframe>
-      <br>
-      <p>-----------------------------------------------------------</p>
-      <br>
-      <h2 style="font-family: 'Urbanist', sans-serif;">4. How to identify fake sneakers</h2><br>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/npgakFPdNeM" frameborder="0" allowfullscreen></iframe>
-      </iframe>
-      <br>
-      <p>-----------------------------------------------------------</p>
-      <br>
 
-   
-    </section>
-    
+    <div class="video-card-container">
+        
+        <div class="video-card">
+            <h2 class="video-title">1. How to choose a perfect size</h2>
+            <p class="video-description">Watch this video to learn how to choose the perfect sneaker size.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/hJXD46OpiA0" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="video-card">
+            <h2 class="video-title">2. How to style Nike Air Force 1</h2>
+            <p class="video-description">Explore styling tips for Nike Air Force 1 sneakers.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/Lp-4TmjsRro" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="video-card">
+            <h2 class="video-title">3. How to take Sneaker Care</h2>
+            <p class="video-description">Learn how to properly care for your sneakers.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/xNQRk76JJLI" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="video-card">
+            <h2 class="video-title">4. How to identify fake sneakers</h2>
+            <p class="video-description">Discover tips for identifying counterfeit sneakers.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/npgakFPdNeM" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+    <div class="video-card-container">
+    <div class="video-card">
+            <h2 class="video-title">1. How to sell your Sneakers</h2>
+            <p class="video-description">Learn many ways how to sell Sneakers properly.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/Pny-tkeloKA" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="video-card">
+            <h2 class="video-title">2. How to lace you Sneakers </h2>
+            <p class="video-description">This video will teach you new lacing techniques.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/dMI5dkPKo4I" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="video-card">
+            <h2 class="video-title">3. How to clean white sneakers</h2>
+            <p class="video-description">Methods to bring your white sneakers back to life again.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/RF86oSB6_bc" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <div class="video-card">
+            <h2 class="video-title">4. How to sneaker customization</h2>
+            <p class="video-description">Discover tips to customize sneakers your own way.</p>
+            <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/lfocB3EdBXo" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div> 
+    <hr>
+    <hr>
+
+    <div class="info">
+    <div>
+    <h1>Sneaker Material Guide</h1>
+    <h4>
+        <ol>
+            <li><b>Leather</b>: Full-grain, synthetic, nubuck/suede.</li>
+            <li><b>Mesh</b>: Breathable, lightweight, used in athletic sneakers.</li>
+            <li><b>Canvas</b>: Durable, classic look (e.g., Converse).</li>
+            <li><b>Synthetic Materials</b>: PU, TPU, neoprene.</li>
+            <li><b>Knit Fabrics</b>: Comfortable, flexible, snug fit.</li>
+            <li><b>Rubber</b>: Outsoles for traction and durability.</li>
+            <li><b>Foam</b>: EVA, PU, various cushioning technologies.</li>
+            <li><b>Gore-Tex</b>: Waterproof, breathable for outdoor use.</li>
+            <li><b>Reflective Materials</b>: Enhance visibility in low light.</li>
+            <li><b>Leatherette/Synthetic Leather</b>: Budget-friendly leather alternatives.</li>
+        </ol>
+    </h4>
+    </div>
+
+    <hr>
+    <hr>
+
+
+    <div>
+    <h1>Sneaker Storage Tips</h1>
+    <h4>
+        <ol>
+            <li>Clean your sneakers and ensure they are dry.</li>
+            <li>Use shoe trees to maintain their shape.</li>
+            <li>Store them in a cool, dry place, away from direct sunlight and humidity.</li>
+            <li>Consider using clear plastic shoeboxes or bags to protect them from dust and light.</li>
+            <li>Avoid overcrowding to allow them to breathe.</li>
+            <li>Rotate your sneakers to extend their lifespan.</li>
+            <li>Check them periodically for cleaning and maintenance.</li>
+            <li>Keep sneakers away from heat sources.</li>
+            <li>Use sneaker protection products for added care.</li>
+            <li>Stack your sneaker boxes carefully to avoid damage.</li>
+        </ol>
+    </h4>
+</div>
+
+<hr>
+<hr>
+
+<div>
+    <h1>How to Attend Sneaker Events</h1>
+    <h4>
+        <ol>
+            <li><b>Research Upcoming Events</b>: Find out about upcoming sneaker events in your area or consider traveling to major sneaker conventions and trade shows.</li>
+            <li><b>Register or Purchase Tickets</b>: Secure your spot by registering for the event or purchasing tickets in advance. Some events may require early registration.</li>
+            <li><b>Plan Your Visit</b>: Prepare your itinerary, including the date, venue location, and event schedule. Ensure you have transportation and accommodations if needed.</li>
+            <li><b>Bring Comfortable Attire</b>: Wear comfortable clothing and sneakers, as you may spend a lot of time on your feet exploring the event. Don't forget your sneaker heat.</li>
+            <li><b>Pack Essentials</b>: Bring essentials such as cash, identification, a backpack for purchases, and any necessary supplies like water and snacks.</li>
+            <li><b>Engage with the Community</b>: Participate in discussions, meet fellow sneakerheads, and engage with brands and sellers. Networking is a key part of the experience.</li>
+            <li><b>Inspect Purchases</b>: If you plan to buy sneakers at the event, inspect them carefully for authenticity and condition before making a purchase.</li>
+            <li><b>Attend Panel Talks and Workshops</b>: Many events feature panel discussions, workshops, and Q&A sessions. Don't miss the chance to learn from experts and insiders.</li>
+            <li><b>Document Your Experience</b>: Take photos and videos to document your experience and share your findings with the sneaker community through social media.</li>
+            <li><b>Respect Event Rules</b>: Follow the event rules and respect the organizers, vendors, and fellow attendees to maintain a positive and enjoyable atmosphere.</li>
+        </ol>
+    </h4>
+</div>
+
+
+
+</section>
+
 </html>
 
 <?php
